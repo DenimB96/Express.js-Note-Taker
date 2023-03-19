293c2c0fd7cca0a883271d65ba19e74a4c8628fa
DenimB96 <rlsjr2@live.com>
2023-03-19 02:21:53 -0400

    HUGE commit, I have been working on this for days and it is finally funtioning correctly. So starters got rid of extra package.json file in the develop folder it was not need I installed all the dependencies needed for the new package.json file in the directory with the server.js file. Set up the server.js file to use express and the file system, set up the middleware, made the get routes for the HTML files, then set up the api routes for the notes and set the notes to be sent to the db.json, and set up a function to delete the notes. I had huge error which halted progress in seeing if my server.js was functional, my stylesheet and index.js were incorrectly sourced and it took entiely toooooooooo long to figure that out. Also added the basic styles for the page.

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "Develop/public")));
+
+// Routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+});
+
+// API routes
+app.get("/api/notes", (req, res) => {
+  const notesData = fs.readFileSync(path.join(__dirname, "/Develop/db/db.json"));
+  const notes = JSON.parse(notesData);
+  res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+  const notesData = fs.readFileSync(path.join(__dirname, "/Develop/db/db.json"));
+  const notes = JSON.parse(notesData);
+
+  const newNote = {
+    id: uuidv4(),
+    title: req.body.title,
+    text: req.body.text,
+  };
+
+  notes.push(newNote);
+  fs.writeFileSync(path.join(__dirname, "/Develop/db/db.json"), JSON.stringify(notes));
+  res.json(newNote);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const notesData = fs.readFileSync(path.join(__dirname, "/Develop/db/db.json"));
+  let notes = JSON.parse(notesData);
+
+  const noteId = req.params.id;
+
+  notes = notes.filter((note) => note.id !== noteId);
+
+  fs.writeFileSync(path.join(__dirname, "/Develop/db/db.json"), JSON.stringify(notes));
+
+  res.json({ message: "Note deleted" });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
